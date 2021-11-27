643f8847fbc53921e75b91ab4a73a0d68384ff41
senoron <perfectbynni@gmail.com>
2021-11-27 22:33:29 +0200

    Added file writing for crypto loader

--- crypto-loader/index.js
+++ crypto-loader/index.js
@@ -1,16 +1,23 @@
 const {PriceGetter} = require('crypto-price-getter');
+const fs = require('fs');
 
+let fileName = 'Prices.json';
 const toSearch = ['BTC', 'ETH', 'SOL', 'ADA', 'DOGE', 'MATIC', 'SHIB', 'LTC',]
 let result = {};
 
 let getPrice = async () => {
+    result.detail = {};
+
     for(let i = 0; i < toSearch.length; i++){
         result.detail[toSearch[i]] = await PriceGetter.getLatestTradePrice(toSearch[i], "USD");
     }
 
     let now = new Date();
 
-    result.detail = JSON.stringify(result);
+    result.lastUpdated = now.toString();
+    fs.writeFile(fileName, JSON.stringify(result),  err =>  {
+        if (err) return console.log(err);
+    });
 
     console.log('updated | ' + now.toString());
 }
