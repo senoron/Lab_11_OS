92a89fd615e9385893d38a434cea28f64d3a0762
Vladyslav <vasa22vladik@gmail.com>
2021-11-30 13:58:53 +0200

    Comments

--- runner/runner/mainwindow.cpp
+++ runner/runner/mainwindow.cpp
@@ -6,6 +6,7 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->lineEdit_state->setText("Not running");
 }
 
 MainWindow::~MainWindow()
@@ -18,13 +19,13 @@ void MainWindow::on_pushButton_run_serveer_clicked()
 {
     pid_t pid = fork();
 
-    if(pid == 0)
+    if(pid == 0)  //Call server`s process
     {
         qDebug() << "I am child-server";
         execvp(argvServer[0] , argvServer);
     }
 
-    if(pid > 0)
+    if(pid > 0)  //Saving server`s pid
     {
         idServer = pid;
         ui->lineEdit_state->setText(StatusServer);
@@ -35,19 +36,19 @@ void MainWindow::on_pushButton_run_serveer_clicked()
 void MainWindow::on_pushButton_clicked()
 {
     CountClients = ui->spinBox_clients->value();
-    idClients.clear();
+    idClients.clear();  //Clearing array with proceses` id
 
     for (int i = 0 ; i < CountClients ; i++ )
     {
         pid_t pid = fork();
 
-        if(pid == 0)
+        if(pid == 0)  //Call client`s process
         {
             qDebug() << "I am child-client";
             execvp(argvClient[0] , argvServer);
         }
 
-        if(pid > 0)
+        if(pid > 0)  //Saving client`s pid
         {
             idClients.push_back(pid);
         }
@@ -59,14 +60,14 @@ void MainWindow::on_pushButton_filltable_clicked()
 {
     ui->list_client->clear();
 
-    for (int i = 0 ; i < CountClients ; i++ )
+    for (int i = 0 ; i < CountClients ; i++ )  //Filling of table
     {
         ui->list_client->addItem("Client " + QString :: number(i + 1) + " , id : " + QString :: number(idClients[i]));
     }
 }
 
 
-void MainWindow::on_pushButton_stop_clicked()
+void MainWindow::on_pushButton_stop_clicked() //Completion of all processes
 {
     for (int i = 0 ; i < CountClients ; i++ )
     {
