c793f92a721336fc8a010e25ae06e8b6a4fdbd20
ighoorr <i.bereshchak@gmail.com>
2021-11-30 11:43:56 +0200

    Add new struct

--- client/client/mainwindow.cpp
+++ client/client/mainwindow.cpp
@@ -13,3 +13,54 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::init(){
+    int handleFile;
+        void* pBuf;
+        const int size = 256;
+        sem_t* semptr = sem_open("sem", O_CREAT ,  0666 , 0);
+        int temp = -2;
+
+        sem_getvalue(semptr , &temp);
+        printf("value sem : %d\n" , temp);
+
+        handleFile = shm_open("file" , O_RDWR , 0666);
+
+        pBuf = mmap(NULL , size , PROT_WRITE | PROT_READ , MAP_SHARED , handleFile , 0);
+
+        int i = 0;
+        while(1)
+        {
+           json lastUpdate;
+            cout<<("I wait\n");
+
+        sem_wait(semptr);
+
+        sem_getvalue(semptr , &temp);
+            printf("value sem : %d\n" , temp);
+
+            printf("My %d msg with server : %s" , i++ + 1 , (char*)pBuf);
+            char* str=(char*)pBuf;
+
+            QJsonDocument jsondocument = QJsonDocument::fromJson(str);
+
+
+
+
+
+            sem_post(semptr);
+
+            sem_getvalue(semptr , &temp);
+            printf("value sem : %d\n" , temp);
+
+            sleep(8);
+
+            if(i == 5)
+                break;
+        }
+
+        //close(handleFile);
+        sem_close(semptr);
+        shm_unlink("file");
+
+
+}
--- client/client/mainwindow.h
+++ client/client/mainwindow.h
@@ -2,6 +2,42 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <fstream>
+#include <iostream>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <semaphore.h>
+
+
+
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QJsonDocument>
+
+#define CountMsg 5
+using namespace std;
+
+struct json{
+    float BTC;
+    float ETH;
+    float SOL;
+    float ADA;
+    float DOGE;
+    float MATIC;
+    float SHIB;
+    float LTC;
+
+    QString update;
+
+
+};
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -14,7 +50,8 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-
+private slots:
+    void init();
 private:
     Ui::MainWindow *ui;
 };
